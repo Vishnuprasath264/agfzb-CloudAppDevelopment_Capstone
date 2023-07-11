967869cfdbf042af1dfd455e944ac6611f3b5aae
Vishnuprasath264 <vishnuprasath264@gmail.com>
2023-07-11 11:30:01 -0400

    Django updates-2

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -26,11 +26,10 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", "localhost:8000",
-                 "afaizan.mybluemix.net"]
+ALLOWED_HOSTS = ["localhost", "localhost:8000"]
 
-CSRF_TRUSTED_ORIGINS = ['https://angelicagard-8000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/',
-                        "http://angelicagard.eu-gb.mybluemix.net",'https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://vishnupras10-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/',
+                        'https://*.127.0.0.1']
 
 # Application definition
 
